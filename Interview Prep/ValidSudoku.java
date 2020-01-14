c51cf14c505afd966bb74400389a1a859af5f25e
Taylan U <taylanu@taylan.me>
2020-01-14 01:15:33 -0500

    Reorganizing and derekBanas C

---
+++ Interview Prep/ValidSudoku.java
@@ -0,0 +1,5 @@
+/*To see if a board is valid, have to ensure that:
+- Each row has no duplicates
+- Each column has no duplicates
+- Each 3x3 square has no duplicates
+*/
---
+++ Sudoku/solver.py
@@ -0,0 +1,14 @@
+#using backtracking as algo.
+# build using backtracking first
+def solver(board):
+    
+
+
+
+
+
+
+
+board = [[5
+
+        ]]
